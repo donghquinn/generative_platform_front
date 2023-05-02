eb125cbd3c2090ecdf006d8e90a5961327957e93
Kim, Dong H <ehdgus1524@gmail.com>
2023-05-02 12:42:29 +0900

    wip

--- components/menu.tsx
+++ components/menu.tsx
@@ -1,36 +1,33 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const menuList = ["Chat", "Image"];
 
 function Menu () {
+    const router = useRouter();
+
     return (
         <div className="navbar bg-base-300 rounded-box">
-  <div className="flex-1 px-2 lg:flex-none">
-    <a className="text-lg font-bold">IMLAB</a>
-  </div> 
-  <div className="flex justify-end flex-1 px-2">
-    {menuList.map((item) => {
+    <div className="flex-1 px-2 lg:flex-none">
+        <a className="text-lg font-bold">IMLAB</a>
+    </div> 
+    <div className="flex justify-end flex-1 px-2">
+        {menuList.map((item) => {
+            if (item ==="Chat") {
+                return(
+                    <div key={item} className="flex items-stretch">
+                            <a className="btn btn-ghost rounded-btn" onClick={() => {router.push("/chat")}}>{item}</a>
+                    </div>
+                )
+            }
 
-        if (item ==="Chat") {
-            return(
-                <div key={item} className="flex items-stretch">
-                    <Link href="/Chat">
-                        <p className="btn btn-ghost rounded-btn">{item}</p>
-                    </Link>
-                
+            if (item === "Image") {
+                return (
+                    <div key={item} className="flex items-stretch">
+                        <a className="btn btn-ghost rounded-btn" onClick={()=> router.push("/img")}>{item}</a>
                 </div>
-            )
-        }
-
-        if (item === "Image") {
-            return (
-                <div key={item} className="flex items-stretch">
-                <Link href="/img">
-                    <p className="btn btn-ghost rounded-btn">{item}</p>
-                </Link>
-            </div>
-            )
-        }
+                )
+            }
         
     })}
 
