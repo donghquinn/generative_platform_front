477deec00d1cd935a8839e04f4f99e8c14d726c3
Kim, Dong H <ehdgus1524@gmail.com>
2023-05-15 12:26:30 +0900

    wip

--- components/upload.tsx
+++ components/upload.tsx
@@ -1,39 +1,40 @@
-import { useState } from "react";
-import Image from "next/image";
 import { useRecoilState } from "recoil";
-import { imageEditFile, imageFileName, imageUpload } from "../src/image.recoil";
+import { imageFileName, imageUpload } from "../src/image.recoil";
 
-function UploadImage() {
+function UploadImage ()
+{
+
     const [selectedImage, setSelectedImage] = useRecoilState(imageUpload);
     const [fileName, setFileName] = useRecoilState(imageFileName);
 
-  const handleImageUpload = (event) => {
-    const file = event.target.files[0];
-    const reader = new FileReader();
+  // const handleImageUpload = (event) => {
+  //   const file = event.target.files[0];
+  //   const reader = new FileReader();
 
-    reader.onload = (event) => {
-      setSelectedImage(event.target.result);
-    };
+  //   reader.onload = (event) => {
+  //     setSelectedImage(event.target.result);
+  //   };
 
-    reader.readAsDataURL(file);
+  //   reader.readAsDataURL(file);
 
-    setFileName(file.name)
+  //   setFileName(file.name)
 
-    console.log(file.name)
-  };
+  //   console.log(file.name)
+  // };
 
-    return(
-        <div>
-          <input type="file" onChange={handleImageUpload} />
-          {selectedImage && (
-            <>
-              <div>
-              {/* <EditImage selectedImage={selectedImage} imgNumber={imgNumber} size={size}></EditImage> */}
-                <Image src={selectedImage} alt="Selected Image" width={300} height={300} />
-              </div>
-        </>
-      )}
-        </div>
+  return (
+      <div>al</div>
+      //   <div>
+      //     <input type="file" onChange={handleImageUpload} />
+      //     {selectedImage && (
+      //       <>
+      //         <div>
+      //         {/* <EditImage selectedImage={selectedImage} imgNumber={imgNumber} size={size}></EditImage> */}
+      //           <Image src={selectedImage} alt="Selected Image" width={300} height={300} />
+      //         </div>
+      //   </>
+      // )}
+      //   </div>
     );
 }
 
--- pages/edit.tsx
+++ pages/edit.tsx
@@ -17,59 +17,65 @@ function ImageUploader() {
   
   
 
-  return (
-    <div>
-      <div className="flex flex-col space-y-2 px-2">
-     <div className="flex justify-center">
-         <div className="dropdown" style={{marginBottom: "1%", marginTop:"5%"}}>
-             <h1 style={{fontSize: "150%"}}>Select Size</h1>
-             <label tabIndex={0} className="btn m-1">
-                 {ment}
-             </label>
-             <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-                 {sizeArray.map((item) => {
-                     return (
-                         <div key={item}>
-                             <li>
-                                <a onClick={(event) => { 
-                                 setSize(item)
-                                 setMent(item)
-                                 }}>{item}</a>
-                             </li>
-                         </div>
-                     )
-                 })}
-             </ul>
-         </div>
-         </div>
+    return (
+          <div>
+          <div className="flex flex-col space-y-2 px-2">
+        <div className="flex justify-center">
+          <p style={{fontSize: "150%", fontWeight: "bold"}}>작업중입니다.</p>
+        </div></div>
+    </div>
+//     <div>
+//       <div className="flex flex-col space-y-2 px-2">
+//      <div className="flex justify-center">
+//          <div className="dropdown" style={{marginBottom: "1%", marginTop:"5%"}}>
+//              <h1 style={{fontSize: "150%"}}>Select Size</h1>
+//              <label tabIndex={0} className="btn m-1">
+//                  {ment}
+//              </label>
+//              <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+//                  {sizeArray.map((item) => {
+//                      return (
+//                          <div key={item}>
+//                              <li>
+//                                 <a onClick={(event) => { 
+//                                  setSize(item)
+//                                  setMent(item)
+//                                  }}>{item}</a>
+//                              </li>
+//                          </div>
+//                      )
+//                  })}
+//              </ul>
+//          </div>
+//          </div>
 
-         <div className="flex flex-col space-y-2 px-2">
-         <div className="flex justify-center">
-         <div className="dropdown" style={{marginBottom: "1%", marginTop:"1%"}}>
-             <h1 style={{fontSize: "150%"}}>Select Number of Images</h1>
-             <label tabIndex={0} className="btn m-1">
-                 {imgMent}
-             </label>
-             <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-                 {imgNumberArray.map((item) => {
-                     return (
-                         <div key={item}>
-                             <li >
-                             <a onClick={(event) => { 
-                                 setNumber(item)
-                                 setImgMent(item)
-                                 }}>{item}</a></li>
-                         </div>
-                     )
-                 })}
-             </ul>
-                </div>
-                </div>
-            <UploadImage></UploadImage>
-         </div>
-        <EditImage size={size} imgNumber={imgNumber}></EditImage>
-      </div>
-   </div>
+//          <div className="flex flex-col space-y-2 px-2">
+//          <div className="flex justify-center">
+//          <div className="dropdown" style={{marginBottom: "1%", marginTop:"1%"}}>
+//              <h1 style={{fontSize: "150%"}}>Select Number of Images</h1>
+//              <label tabIndex={0} className="btn m-1">
+//                  {imgMent}
+//              </label>
+//              <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+//                  {imgNumberArray.map((item) => {
+//                      return (
+//                          <div key={item}>
+//                              <li >
+//                              <a onClick={(event) => { 
+//                                  setNumber(item)
+//                                  setImgMent(item)
+//                                  }}>{item}</a></li>
+//                          </div>
+//                      )
+//                  })}
+//              </ul>
+//                 </div>
+//                 </div>
+//             <UploadImage></UploadImage>
+//          </div>
+//         <EditImage size={size} imgNumber={imgNumber}></EditImage>
+//       </div>
+//    </div>
   );
 }
 
