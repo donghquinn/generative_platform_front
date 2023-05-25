2ea3b25b5b191fb58b5674891896bde5ecdee33b
Kim, Dong H <ehdgus1524@gmail.com>
2023-05-25 17:24:26 +0900

    wip

---
+++ components/signin.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function SignIn ()
+{
+    const [ email, setEmail ] = useState<string>();
+    const [ password, setPassword ] = useState<string>();
+}
+
+export default SignIn;
