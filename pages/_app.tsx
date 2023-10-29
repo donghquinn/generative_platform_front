a95d8f54c15f7f7f008fb0001c0ea337ff232c6a
donghquinn <ehdgus1524@gmail.com>
2023-10-30 00:25:05 +0900

    wip

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,24 +17,30 @@ export const metadata: Metadata = {
     url: 'https://chat.andongh.com',
     siteName: 'chat',
     description: "Chat with OpenAi's chat-GPT"
-  },
-  
+  }, 
 }
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+function MyApp({
+  children,
+}: {
+  children: React.ReactNode;
+})  {
   return (
-    <>
+    <html>
       <Head>
         <title>IMLAB ChatGPT</title>
-        <metadata ></metadata>
+
       </Head>
       <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4259757759902386"
      crossOrigin="anonymous"></Script>
+        
         <RecoilRoot>
-        <Menu></Menu>
-          <Component {...pageProps} />
+          <body>
+          <Menu></Menu>
+            {children}
+          </body>
         </RecoilRoot>
-    </>
+    </html>
   );
 }
 
