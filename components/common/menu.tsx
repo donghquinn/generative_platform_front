8cdb3d223be9db598e8b5d0b9e210c441d9f33dd
Kim Donghyun <kimdonghyun@Kims-MacBook-Air.local>
2023-12-01 16:10:35 +0900

    wip: resolution

--- components/common/menu.tsx
+++ components/common/menu.tsx
@@ -1,24 +1,27 @@
 import { useRouter } from "next/router";
 import Image from "next/image";
 import logo from '../../public/logo.png';
+import Link from "next/link";
 
 // Menu Component
-const menuList = ["Chat", "Image", "Edit"];
+const menuList = ["Chat", "Image", "Edit", "S-R"];
 // className="text-lg font-bold btn"
 function Menu () {
-    const router = useRouter();
 
     return (
         <div className="navbar bg-base-300 rounded-box">
             <div className="flex-1 px-2 lg:flex-none">
-                <Image src={logo} alt="logo" width={50} height={50} onClick={() =>  router.push("/")}/>
+                <Link href="/">
+                    <Image src={logo} alt="logo" width={50} height={50} />
+                </Link>
+                
             </div> 
             <div className="flex justify-end flex-1 px-2">
             {menuList.map((item) => {
                 if (item ==="Chat") {
                     return(
                         <div key={item} className="flex items-stretch">
-                                <a className="btn btn-ghost rounded-btn" onClick={() => {router.push("/chat")}}>{item}</a>
+                                <Link className="btn btn-ghost rounded-btn" href={"/chat"}>{item}</Link>
                         </div>
                     )
                 }
@@ -26,18 +29,18 @@ function Menu () {
                 if (item === "Image") {
                     return (
                         <div key={item} className="flex items-stretch">
-                            <a className="btn btn-ghost rounded-btn" onClick={() => router.push("/generate")}>{item}</a>
+                                <Link className="btn btn-ghost rounded-btn" href={"/generate"}>{item}</Link>
                         </div>
                     )
                 }
 
-                // if (item ==="Edit") {
-                //     return (
-                //         <div key={item} className="flex items-stretch">
-                //             <a className="btn btn-ghost rounded-btn" onClick={() => router.push("/edit")}>{item}</a>
-                //         </div>
-                //     )
-                // }
+                if (item ==="S-R") {
+                    return (
+                        <div key={item} className="flex items-stretch">
+                            <Link className="btn btn-ghost rounded-btn" href={"/resolution"}>{item}</Link>
+                        </div>
+                    )
+                }
             })}
         </div>
     </div>
