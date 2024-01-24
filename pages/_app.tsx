480270385375d2e8da98be83bd23ebd7c9841d5f
donghquinn <ehdgus1524@gmail.com>
2024-01-25 02:09:44 +0900

    wip

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -44,13 +44,11 @@ function MyApp ({ Component, pageProps: { session, ...pageProps } })
         <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4259757759902386"
         crossOrigin="anonymous"></Script>
       <RecoilRoot>
-        <div className='bg-white'>
           <MantineProvider theme={theme}>
           <Menu></Menu>
           <Component {...pageProps} />
             <Footer></Footer>
           </MantineProvider>
-        </div>
         </RecoilRoot>
     </>
   );
