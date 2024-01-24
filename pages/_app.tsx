8877971613f253167c0a8d32090c1dab9786f8f1
donghquinn <ehdgus1524@gmail.com>
2024-01-25 02:14:25 +0900

    wip

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import '../styles/global.css';
 import '@mantine/core/styles.css';
+import '@mantine/core/styles/UnstyledButton.css';
+import '@mantine/core/styles/Button.css';
 
 import { createTheme, MantineProvider } from '@mantine/core';
 import { Metadata } from 'next';
