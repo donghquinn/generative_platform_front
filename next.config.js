c691696b8bf444ea04437b59c1a6ed6b93ed329a
donghquinn <ehdgus1524@gmail.com>
2024-01-25 00:37:09 +0900

    wip

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
    images: {
     domains: ['oaidalleapiprodscus.blob.core.windows.net'],
   },
+   experimental: {
+    // this includes files from the monorepo base two directories up
+    outputFileTracingRoot: path.join(__dirname, '../../'),
+  },
 }
 
 module.exports = nextConfig
