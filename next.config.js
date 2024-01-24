173f142316e9acd68eb44c02a24f90a9f662b369
donghquinn <ehdgus1524@gmail.com>
2024-01-25 00:49:11 +0900

    wip: docker-compsoe

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: "standalone",
-   images: {
+  images: {
     domains: ['oaidalleapiprodscus.blob.core.windows.net'],
   },
   //  experimental: {
