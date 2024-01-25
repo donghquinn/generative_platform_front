744113a669ce1a039d7692f6fd7a65dce9d5e76c
donghquinn <ehdgus1524@gmail.com>
2024-01-25 23:32:12 +0900

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,7 @@
 module.exports = {
   content: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      gridTemplateColumns: {
-        sidebar: "300px auto"
-      },
-      gridTemplateRows: {
-        header: "64px auto"
-      }
-    },
+    extend: {},
   }
 }
 
