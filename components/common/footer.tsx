1cfa21d5b6277f8d279f2900d2720ee669216227
donghquinn <ehdgus1524@gmail.com>
2024-02-11 17:34:09 +0900

    wip: chaing github

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -7,8 +7,10 @@ function Footer() {
     <footer className="footer p-10 bg text-content">
             <div className="flex flex-col space-y-2 px-2">
                 <div className="flex justify-center">
-        <Link href="https://github.com/donghquinn" style={{marginRight: "1%", marginBottom: "1%"}}>
-          <Image width={'30'} height="20" alt="github" src={github}/>
+          <Link href="https://github.com/donghquinn/generative_platform_front"
+            style={{ marginRight: "1%", marginBottom: "1%" }}
+          >
+            <Image width={'30'} height="20" alt="github" src={github}/>
         </Link>
         <p style={{color: "black"}}>
           donghquinn
@@ -17,11 +19,11 @@ function Footer() {
         </div>
         </div>
             <div className="flex flex-col space-y-2 px-2">
-                <div className="flex justify-center">
-        <span className="footer-title" style={{color: "black"}}>Contact</span>
-        <p style={{color: "black"}}>ehdgus1524@gmail.com</p>
-        </div>
-        </div>
+              <div className="flex justify-center">
+                <span className="footer-title" style={{color: "black"}}>Contact</span>
+                <p style={{color: "black"}}>ehdgus1524@gmail.com</p>
+              </div>
+          </div>
     </footer>
   );
 }
